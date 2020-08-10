56e1e1696abef4b6d0d5ccab62236630cd1fdc3a
joaogqueiroz <joaogilberto350@hotmail.com>
2020-08-10 19:15:52 -0300

    API

--- Api_Rest_CRUD/app.py
+++ Api_Rest_CRUD/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from resource.hotel import Hoteis
+from resource.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,6 +8,8 @@ api = Api(app)
 
 
 api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel,'/hoteis/<string:hotel_id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
