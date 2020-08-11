8af09d6b22e4e5517daac2f77494dde06fe96344
joaogqueiroz <joaogilberto350@hotmail.com>
2020-08-10 21:29:00 -0300

    CRUD completo + ajuste dos arquivos

--- Api_Rest_CRUD/resource/hotel.py
+++ Api_Rest_CRUD/resource/hotel.py
@@ -1,23 +1,23 @@
 from flask_restful import Resource, reqparse
-
+from models.hotel import HotelModel
 hoteis = [
         {
         'hotel_id': 'alpha',
-        'nome:': 'Alpha Hotel',
+        'nome': 'Alpha Hotel',
         'estrelas': 4.3,
         'diaria':420.34,
         'cidade':'Rio de Janeiro'
         },
         {
         'hotel_id': 'bravo',
-        'nome:': 'Bravo Hotel',
+        'nome': 'Bravo Hotel',
         'estrelas': 5.0,
         'diaria':5000,
         'cidade':'Niteroi'
         },
         {
         'hotel_id': 'charlie',
-        'nome:': 'Charlie Hotel',
+        'nome': 'Charlie Hotel',
         'estrelas': 4.4,
         'diaria':380.34,
         'cidade':'Santa Catarina'
@@ -29,6 +29,7 @@ class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
+
 class Hotel(Resource):
 
     argumentos = reqparse.RequestParser()
@@ -36,6 +37,7 @@ class Hotel(Resource):
     argumentos.add_argument('estrelas')
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
+
 
 
     def find_hotel(hotel_id):
@@ -55,7 +57,9 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = { 'hotel_id':hotel_id, **dados }
+        novo_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = novo_objeto.json()
+        #novo_hotel = { 'hotel_id':hotel_id, **dados }
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
@@ -64,8 +68,8 @@ class Hotel(Resource):
     def put(self, hotel_id):
 
          dados = Hotel.argumentos.parse_args()
-         novo_hotel = { 'hotel_id':hotel_id, **dados }
-
+         novo_objeto = HotelModel(hotel_id, **dados)
+         novo_hotel = novo_objeto.json()
          hotel = Hotel.find_hotel(hotel_id)
          if hotel:
              hotel.update(novo_hotel)
@@ -74,4 +78,6 @@ class Hotel(Resource):
          return novo_hotel, 201
 
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted. '}
