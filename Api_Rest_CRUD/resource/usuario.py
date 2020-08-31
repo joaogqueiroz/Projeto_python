36b3714eba992e1391a469d661477e0757496584
joaogqueiroz <joaogilberto350@hotmail.com>
2020-08-31 02:28:34 -0300

    Autenticação de cadastro por e-mail com mailgun

--- Api_Rest_CRUD/resource/usuario.py
+++ Api_Rest_CRUD/resource/usuario.py
@@ -1,12 +1,16 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask import make_response, render_template
 from flask_jwt_extended import create_access_token, jwt_required, get_raw_jwt
 from werkzeug.security import safe_str_cmp
 from blacklist import BLACKLIST
+import traceback
+
 
 argumentos = reqparse.RequestParser()
 argumentos.add_argument('login', type=str, required=True, help="The fiel 'login' cannot be left blank.")
 argumentos.add_argument('senha', type=str, required=True, help="The fiel 'senha' cannot be left blank.")
+argumentos.add_argument('email', type=str)
 argumentos.add_argument('ativado', type=bool)
 
 class User(Resource):
@@ -29,12 +33,23 @@ class UserRegister(Resource):
     #/cadastro
     def post(self):
         dados = argumentos.parse_args()
+        if not dados.get('email') or dados.get('email') is None:
+            return{"messege": "The field 'email' cannot be left blank"},400
+
+        if UserModel.find_by_email(dados['email']):
+            return{"messege": "The email {} already existis".format(dados['email'])},400
 
         if UserModel.find_by_login(dados['login']):
-            return{"messege": "The login '{}' already existis.".format(dados['login'])}
+            return{"messege": "The login '{}' already existis.".format(dados['login'])}, 400
         user = UserModel(**dados)
         user.ativado = False
-        user.save_user()
+        try:
+            user.save_user()
+            user.send_confirmation_email()
+        except:
+            user.delete_user()
+            traceback.print_exc()
+            return{'messege': 'An internal server erro has ocurred.'}, 500
         return{'messege': 'User created successfully!'}, 201
 
 class UserLogin(Resource):
@@ -68,4 +83,6 @@ class UserConfirm(Resource):
             return{"messege" : "User id '{}' not found".format(user_id)}, 404
         user.ativado = True
         user.save_user()
-        return{"messege": "User id '{}' confirmed successfully.".format(user_id)}
+        #return{"messege": "User id '{}' confirmed successfully.".format(user_id)}, 200
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('user_confirm.html', email=user.email, usuario= user.login),200)
